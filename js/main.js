3bd9de87af38a2c87d2c5fa87b040090291aacdd
Ahmed Yasser <68778135+immodi@users.noreply.github.com>
2024-12-22 17:00:11 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,19 +2,27 @@
 async function sendUserIP() {
     try {
       // Fetch the user's IP address
+      const TELEGRAM_API_TOKEN = process.env.TELEGRAM_API_TOKEN;
+      const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
+
       const ipResponse = await fetch('https://api.ipify.org?format=json');
       const ipData = await ipResponse.json();
       const userIP = ipData.ip;
   
       // Send the IP to your Netlify function
-      const res = await fetch('/.netlify/functions/send-ip', {
+  
+      const url = `https://api.telegram.org/bot${TELEGRAM_API_TOKEN}/sendMessage`;
+  
+      const telegramResponse = await fetch(url, {
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ ip: userIP }),
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          chat_id: TELEGRAM_CHAT_ID,
+          text: `New visitor IP: ${userIP}`,
+        }),
       });
-      console.log(res.json());
+      
+      console.log(telegramResponse.json())
       
     } catch (error) {
       console.error('Error fetching or sending IP:', error);
